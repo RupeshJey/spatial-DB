091dd4b5cf9f6deafd97f6df552deb0a6cba03c3
Rupesh Jeyaram <rjeyaram@caltech.edu>
2019-06-19 17:06:51 -0700

    Implement creation of shapes table

---
+++ SQL_Scripts/make_shape_tables.sql
@@ -0,0 +1,34 @@
+-- Rupesh Jeyaram 
+-- Created June 19th, 2019
+
+-- DROP TABLE statements
+
+DROP TABLE IF EXISTS shapes;
+
+-- CREATE TABLE statements
+
+-- shapes holds all outline data from the GeoJSON files
+
+CREATE TABLE shapes (
+
+    -- Auto-incrementing shape id
+    shape_id        SERIAL, 
+
+    -- Name of shape
+    name            VARCHAR(25)          NOT NULL,
+
+    -- Type of shape (country / state / county)
+    type            VARCHAR(10)          NOT NULL
+
+    -- Make sure that the type is legitimate (country or state or county)
+                    CHECK (type = 'Country' OR 
+                           type = 'State' OR 
+                           type = 'County'),
+
+    -- Shape itself
+    shape           GEOMETRY             NOT NULL,       
+    
+    -- The id is enough to uniquely identify the data point
+    PRIMARY KEY (shape_id)
+    
+);
