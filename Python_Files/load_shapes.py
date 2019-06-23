40aa29bc2ec0a5c210a4992513161fc007e3ac41
Rupesh Jeyaram <rjeyaram@caltech.edu>
2019-06-22 18:08:36 -0700

    Implement county-level shapes insertion

--- Python_Files/load_shapes.py
+++ Python_Files/load_shapes.py
@@ -2,9 +2,8 @@
 # Created June 19th, 2019
 
 # Load all the US shape data from the GeoJSON files in SOURCE
-# Currently there are three general types: 
+# Currently there are two general types: 
 # 
-#   - Country level (only US)
 #   - State level 
 #   - County level 
 # 
@@ -14,16 +13,19 @@ import json                     # For parsing shapes
 import psycopg2 as dbapi        # For connecting to database
 import time                     # For timing the insertion
 import sys                      # For printing status
+import csv                      # For parsing county files
+import re                       # For string-parsing the JSON in csv 
 
 SOURCE = '../Shape_Outlines/'   # Where to find the data
 
 # Specific files to use
 COUNTRY_FILE = 'gz_2010_us_outline.json'
 STATES_FILE = 'gz_2010_us_states.json'
-COUNTY_FILE = 'gz_2010_us_counties.json'
+COUNTY_FILE = 'us_county.csv'
 
 # In case this changes... 
-NUM_STATES = 50
+NUM_STATES = 52         # This includes Puerto Rico and D.C. separately 
+NUM_COUNTIES = 3141
 
 # Connect to database
 conn = dbapi.connect(host= '127.0.0.1', 
@@ -35,9 +37,13 @@ conn = dbapi.connect(host= '127.0.0.1',
 # Cursor to execute queries and read output
 cursor = conn.cursor()
 
-# Clear database first (temporary line)
+# Set up the database itself
 os.system("PGPASSWORD=frankenberg psql -U postgres -d SIF_Experiments -f \
            ../SQL_Scripts/make_shape_tables.sql ")
+
+########################################################################
+# INSERT STATES
+########################################################################
 
 # First insert the US states
 file = SOURCE + STATES_FILE
@@ -84,9 +90,6 @@ for i in range(NUM_STATES):
     # Increment state counter
     state_num += 1
 
-# Commit all changes to the database
-conn.commit()
-
 # Clear the output
 sys.stdout.write(" " * 50)
 sys.stdout.flush()
@@ -94,3 +97,70 @@ sys.stdout.write("\r")
 
 # Notify user of completion
 print("Done inserting states!")
+
+########################################################################
+# INSERT COUNTIES
+########################################################################
+
+# Change file to look at counties
+file = SOURCE + COUNTY_FILE
+
+# Notify user
+print("Loading counties file...")
+
+# Which number county we are working on
+county_num = 1; 
+
+# Open the csv file
+with open(file, newline='') as csvfile:
+
+    # Create a reader for this csv file
+    county_reader = csv.reader(csvfile, delimiter='\n')
+
+    # Loop through each row in the csv file
+    for row in county_reader:
+
+        # Extract the name and geometry of the county
+        name = row[0].split(',')[0].replace("'", "")
+        geom = '{' + re.search('{(.*)}', row[0]).group(1) + '}'
+        geom = json.loads(geom.replace('\"\"', '\"'))
+
+        # Clear the last line
+        sys.stdout.write(" " * 60)
+        sys.stdout.flush()
+        sys.stdout.write("\r")
+
+        # Notify the user about progress
+        sys.stdout.write(("Inserting county %i/%i: " % 
+                            (county_num, NUM_COUNTIES)) + name)
+        sys.stdout.flush()
+
+        # Construct SQL query to insert the shape
+        cmd = 'INSERT INTO shapes VALUES (DEFAULT, \'%s\', \'%s\', \
+                       ST_GeomFromGeoJSON(\'%s\'));' % \
+                       (name, "County", str(geom).replace("\'", "\""))
+
+        # Execute that query
+        cursor.execute(cmd)
+
+        # Move cursor to start of line
+        sys.stdout.write('\r')
+
+        # Increment county counter
+        county_num += 1
+
+# Clear the output
+sys.stdout.write(" " * 60)
+sys.stdout.flush()
+sys.stdout.write("\r")
+
+# Commit all changes to the database
+
+# Doing this here so that states + counties are committed together 
+# If something breaks in between, it would otherwise leave the shapes
+# without any counties. 
+
+conn.commit()
+
+# Notify user of completion
+print("Done inserting counties!")
