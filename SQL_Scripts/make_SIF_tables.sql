d23efd3456c1b3f3c52526a43afe43fcabb5fc38
Rupesh Jeyaram <rjeyaram@caltech.edu>
2019-06-26 18:40:19 -0700

    Fix appropriate precision for each data point

--- SQL_Scripts/make_SIF_tables.sql
+++ SQL_Scripts/make_SIF_tables.sql
@@ -11,22 +11,28 @@ DROP TABLE IF EXISTS tropomi_SIF;
 
 CREATE TABLE tropomi_SIF (
 
-    -- Time at which data was obtained
-    time            TIMESTAMP            NOT NULL, 
+    -- Unique record id
+    record_id       SERIAL              NOT NULL,
 
-    -- Parameters of the data point. All assumed to be NUMERIC(10, 7) for now..
-    sif             NUMERIC(10, 7)       NOT NULL, 
-    sif_err         NUMERIC(10, 7)       NOT NULL,
-    sif_relative    NUMERIC(10, 7)       NOT NULL,
-    dcSIF           NUMERIC(10, 7)       NOT NULL,
-    cloud_fraction  NUMERIC(10, 7)       NOT NULL,
-    sza             NUMERIC(10, 7)       NOT NULL,
-    vza             NUMERIC(10, 7)       NOT NULL,
-    phase_angle     NUMERIC(10, 7)       NOT NULL,
-    dcf             NUMERIC(10, 7)       NOT NULL,     
+    -- Time at which data was obtained
+    time            TIMESTAMP           NOT NULL, 
+
+    -- Parameters of the data point
+    sif             NUMERIC(5, 3)       NOT NULL, 
+    sif_err         NUMERIC(4, 3)       NOT NULL,
+    sif_relative    NUMERIC(5, 2)       NOT NULL,
+    dcSIF           NUMERIC(5, 3)       NOT NULL,
+    cloud_fraction  NUMERIC(3, 2)       NOT NULL,
+    sza             NUMERIC(3, 1)       NOT NULL,
+    vza             NUMERIC(3, 1)       NOT NULL,
+    phase_angle     NUMERIC(4, 1)       NOT NULL,
+    dcf             NUMERIC(5, 4)       NOT NULL,     
 
     -- Geometric properties of the data record
-    center_pt       GEOMETRY             NOT NULL,  -- Data's center point
-    mbr             GEOMETRY             NOT NULL   -- Data's bounding rectangle
+    center_pt       GEOMETRY            NOT NULL,  -- Data's center point
+    mbr             GEOMETRY            NOT NULL,  -- Data's bounding rectangle
+
+    -- Uniquely identifies a data record
+    PRIMARY KEY (record_id)
 );
 
