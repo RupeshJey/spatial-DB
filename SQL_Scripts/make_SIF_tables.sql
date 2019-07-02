5c2a81b44d0eed35e90f129caf5cb23e237ccb90
Rupesh Jeyaram <rjeyaram@caltech.edu>
2019-07-01 21:26:34 -0400

    Enable spatial and temporal indexes on table creation

--- SQL_Scripts/make_SIF_tables.sql
+++ SQL_Scripts/make_SIF_tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE tropomi_SIF (
     time            TIMESTAMP           NOT NULL, 
 
     -- Parameters of the data point
-    sif             NUMERIC(5, 3)       NOT NULL, 
+    sif             NUMERIC(5, 3)       NOT NULL,
     sif_err         NUMERIC(4, 3)       NOT NULL,
     sif_relative    NUMERIC(5, 2)       NOT NULL,
     dcSIF           NUMERIC(5, 3)       NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE tropomi_SIF (
     sza             NUMERIC(3, 1)       NOT NULL,
     vza             NUMERIC(3, 1)       NOT NULL,
     phase_angle     NUMERIC(4, 1)       NOT NULL,
-    dcf             NUMERIC(5, 4)       NOT NULL,     
+    dcf             NUMERIC(5, 4)       NOT NULL,
 
     -- Geometric properties of the data record
     center_pt       GEOMETRY            NOT NULL,  -- Data's center point
@@ -35,4 +35,21 @@ CREATE TABLE tropomi_SIF (
     -- Uniquely identifies a data record
     PRIMARY KEY (record_id)
 );
+
+-- CREATE INDEX statements 
+
+-- Note: creating the index here instead of at the end slows down 
+-- insertion, BUT it allows us to monitor the index's progress. If 
+-- called at the end, the CREATE INDEX statements run w/o progress
+-- updates, and we do not know how long the statements will take. 
+-- Tradeoffs! 
+
+-- Index on the center points of the data
+CREATE INDEX center_points_index
+  ON tropomi_sif
+  USING GIST (center_pt);
+
+-- Index on the timestamp of the data
+CREATE INDEX time_idx
+  ON tropomi_sif (time);
 
