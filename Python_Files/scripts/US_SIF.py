c3b7de560012abe0d0586bd319a303d43b2d1c6a
Rupesh Jeyaram <rjeyaram@caltech.edu>
2019-07-22 17:01:43 -0700

    Add basemap to visualization

--- Python_Files/scripts/US_SIF.py
+++ Python_Files/scripts/US_SIF.py
@@ -3,20 +3,15 @@
 
 # This file handles all the code for the US SIF visualizations tab
 
-import psycopg2 as dbapi          # For connecting to database
-import json                       # For parsing shapes
-import geopandas as gpd           # For extracting data in a geo framework
-import time                       # For timing
-import matplotlib.pyplot as plt   # For plotting the data
-from datetime import date         # For date queries
-from datetime import datetime     # For converting utc / date
+import geopandas as gpd                # For extracting data in a geo framework
+import time                            # For timing
+from datetime import date              # For date queries
+from db_utils import *                 # For easier querying
 
-from bokeh.io import show                           # For showing the plot
 from bokeh.models import LinearColorMapper          # For mapping values/colors
 from bokeh.models import ColumnDataSource           # For holding data
 from bokeh.palettes import Viridis256 as palette    # The map palette to use
 from bokeh.palettes import Category10 as time_pltt  # Time srs palette to use
-import itertools                                    # Iter through colors (plt)
 from bokeh.plotting import figure, curdoc           # For creating the map
 from bokeh.events import Tap                        # For recognizing tap
 from bokeh.layouts import row, column               # For arranging view in grid
@@ -24,20 +19,11 @@ from bokeh.models import ColorBar, FixedTicker      # For map's color bar
 from bokeh.models import DateSlider, DateRangeSlider# For date selection
 from bokeh.models import Panel                      # For assigning grid to view
 
+# For mapping
+from bokeh.tile_providers import get_provider, Vendors
+
 import shapely          # For checking shape type (Polygon/Multipolygon)
 import numpy as np      # Converting lists to np lists (bugs out otherwise)
-
-# For getting the connection details (stored abstractly)
-from connection_info import conn as conn_info
-
-# Connect to database
-conn = dbapi.connect(port= conn_info['port'], 
-                     user= conn_info['user'],
-                     password= conn_info['password'], 
-                     database= conn_info['database'])
-
-# Cursor to execute queries and read output
-cursor = conn.cursor()
 
 # Function that generates the US visualization and passes the tab to main
 def US_SIF_tab():
@@ -46,44 +32,16 @@ def US_SIF_tab():
     # Set up date slider
     #################################
 
-    # Get the date range from the table
-    def get_date_range():
-        
-        # Query to obtain first and last day recorded
-        cmd = 'SELECT MIN(day) AS min, MAX(day) AS max \
-               FROM county_day_sif_facts'
-
-        # Execute query
-        cursor.execute(cmd)
-
-        # Obtain results
-        for min_val, max_val in cursor:
-            min_date = min_val
-            max_date = max_val
-
-        # Return the range
-        return (min_date, max_date)
-
     # create a callback for when the date slider is changed
     def date_selected(attr, old, new):
 
         t0 = time.time()
 
         # Get the new day's data
-        sifs, shapes = get_data_for_day(new)
+        sifs = get_data_for_day(new)
 
-        # For every shape (county) in the county_aggregates 
-        county_num = 0
-        for s in counties["shape"].values:
-
-            # If this is a simple polygon 
-            if (type(s) != shapely.geometry.polygon.Polygon):
-                # List of polygons
-                ps = list(s)
-                for i in range(len(ps) - 1):
-                    sifs = np.append(sifs, sifs[county_num])
-            else:
-                county_num += 1
+        # Match sifs to the multi-polygons format
+        sifs = multify(counties["shapes_simple"].values, sifs)
 
         # Update the sif values
         source.data["sifs"] = np.array(sifs)
@@ -101,6 +59,7 @@ def US_SIF_tab():
         start=start_date, end=end_date, step=1)
 
     # Assign the callback for when the date slider changes
+    date_slider.callback_policy = "mouseup"
     date_slider.on_change('value', date_selected)
 
     #################################
@@ -112,26 +71,13 @@ def US_SIF_tab():
     def get_data_for_date_range(start_date, end_date):
 
         # Command to retrieve the day's county-wise averages
-        cmd = 'SELECT AVG(sif_avg), shape \
-               FROM county_day_sif_facts NATURAL JOIN shapes\
+        cmd = 'SELECT AVG(sif_avg) \
+               FROM county_day_sif_facts\
                WHERE day BETWEEN \'%s\' AND \'%s\' \
-               AND ST_XMax(shape) > -127 \
-               AND type = \'County\'\
-               GROUP BY shape_id, shape\
+               GROUP BY shape_id \
                ORDER BY shape_id' % (start_date, end_date)
 
-        # Execute the query
-        cursor.execute(cmd)
-
-        # Parcel up the results and return them
-        sifs = []
-        shapes = []
-
-        for sif, shape in cursor:
-            sifs.append(sif)
-            shapes.append(shape)
-
-        return (np.array(sifs), shapes)
+        return (np.array(query_db(cmd)))
 
     # create a callback for when the date slider is changed
     def date_range_selected(attr, old, new):
@@ -142,41 +88,34 @@ def US_SIF_tab():
         range_start, range_end = new
 
         # Convert to SQL format
-        range_start = datetime.utcfromtimestamp(range_start/1000).strftime('%Y-%m-%d')
-        range_end = datetime.utcfromtimestamp(range_end/1000).strftime('%Y-%m-%d')
-
+        range_start = utc_from_timestamp(range_start)
+        range_end = utc_from_timestamp(range_end)
+        
         # Get the new day's data
-        sifs, shapes = get_data_for_date_range(range_start, range_end)
-
-        # For every shape (county) in the county_aggregates 
-        county_num = 0
-        for s in counties["shape"].values:
-
-            # If this is a simple polygon 
-            if (type(s) != shapely.geometry.polygon.Polygon):
-                # List of polygons
-                ps = list(s)
-                for i in range(len(ps) - 1):
-                    sifs = np.append(sifs, sifs[county_num])
-            else:
-                county_num += 1
+        sifs = get_data_for_date_range(range_start, range_end)
+        
+        # Match sifs to the multi-polygons format
+        sifs = multify(counties["shapes_simple"].values, sifs)
 
         # Update the sif values
         source.data["sifs"] = np.array(sifs)
 
         # Set the title of the map to reflect the selected date
-        p.title.text = "SIF Average by County: %s to %s" % (range_start, range_end)
+        p.title.text = "SIF Average by County: %s to %s" % (range_start, 
+                                                            range_end)
 
-        print("Took " + str(time.time() - t0) + " seconds to retrieve new data")
+        print("Took " + str(time.time() - t0) + " seconds to update")
 
     # Create the date slider
     date_range_slider = DateRangeSlider(title="Date Range: ", 
                                         start=start_date, end=end_date, 
                                         value=(date(2018, 9, 7), 
-                                            date(2018, 10, 15)), step=1)
+                                            date(2018, 9, 17)), step=1)
 
     # Assign the callback for when the date slider changes
-    date_range_slider.on_change('value', date_range_selected)
+    date_range_slider.callback_policy = "throttle"
+    date_range_slider.callback_throttle = 200
+    date_range_slider.on_change('value_throttled', date_range_selected)
 
     #################################
     # Set up the map and its source
@@ -186,34 +125,25 @@ def US_SIF_tab():
     def get_data_for_day(day):
 
         # Command to retrieve the day's county-wise averages
-        cmd = 'SELECT sif_avg, shape \
+        cmd = 'SELECT sif_avg \
                FROM county_day_sif_facts NATURAL JOIN shapes\
                WHERE day = \'%s\' \
-               AND ST_XMax(shape) > -127 \
                AND type = \'County\'\
                ORDER BY shape_id' % (day)
         
-        # Execute the query
-        cursor.execute(cmd)
+        # Obtain results
+        results = query_db(cmd)
 
-        # Parcel up the results and return them
-        sifs = []
-        shapes = []
-
-        for sif, shape in cursor:
-            sifs.append(sif)
-            shapes.append(shape)
-
-        return (np.array(sifs), shapes)
+        # Return sif_averages and shapes
+        return np.array(results)
 
     # Command to retrieve all the county shapes
     cmd = 'SELECT * FROM shapes WHERE type = \'County\' \
-           AND ST_XMax(shape) > -127 \
            ORDER BY shape_id'
 
     # Execute query and obtain results
     counties = gpd.GeoDataFrame.from_postgis(cmd, conn, 
-                                             geom_col = 'shape')
+                                             geom_col = 'shapes_simple')
 
     # Extract names
     county_names = counties["name"].values
@@ -230,10 +160,10 @@ def US_SIF_tab():
     county_num = 0
 
     # Get the initial sif values (just whatever the last day gives)
-    county_sifs, z = get_data_for_day(end_date)
+    county_sifs = get_data_for_day(end_date)
 
     # For every shape (county) in the county_aggregates 
-    for s in counties["shape"].values:
+    for s in counties["shapes_simple"].values:
 
         # If this is a simple polygon 
         if (type(s) == shapely.geometry.polygon.Polygon):
@@ -279,6 +209,22 @@ def US_SIF_tab():
     county_xs = np.append(county_xs, county_xs_appended)
     county_ys = np.append(county_ys, county_ys_appended)
 
+    print(np.shape(county_xs))
+    print(np.shape(county_ys))
+
+    # Convert to mercator projection
+    county_xs, county_ys = convert_shapes_to_mercator(county_ys, county_xs)
+
+    # r_major = 6378137.000
+
+    # redone_county_ys = []
+
+    # for x, y in zip(county_xs, county_ys):
+    #     scale = x_new / y
+    #     y_new = 180.0/math.pi * math.log(math.tan(math.pi/4.0 + 
+    #             county_ys * (math.pi/180.0)/2.0)) * scale
+    #     redone_county_ys.append(y_new)
+
     # Dictionary to hold the data
     source=ColumnDataSource(data = dict(
         x= np.array(county_xs),
@@ -290,6 +236,9 @@ def US_SIF_tab():
     # Which tools should be available to the user
     TOOLS = "pan,wheel_zoom,reset,hover,save,tap"
 
+    # Obtain map provider
+    tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
+
     # Configure the figure
     p = figure(
         title="SIF Average by County: %s" % end_date, tools=TOOLS,
@@ -300,9 +249,17 @@ def US_SIF_tab():
             ("Average SIF", "@sifs"), 
             ("(Long, Lat)", "($x, $y)")
         ],
+        x_axis_type='mercator',
+        y_axis_type='mercator',
         plot_height = 600,
         plot_width = 1200,
         output_backend="webgl")
+
+    # Add the map!
+    p.add_tile(tile_provider)
+
+    # No downsampling
+    p.lod_threshold = None
 
     # No logo
     p.toolbar.logo = None
@@ -336,14 +293,14 @@ def US_SIF_tab():
     # create a callback that will plot the time series of a selected county
     def county_clicked(event):
 
-        # Obtain click information
-        coord_x = event.x
-        coord_y = event.y
+        # Obtain click information w.r.t. lat/lon
+        coord_x, coord_y = to_lat_lon(event.y, event.x)
 
         # Command that requests SIF time-series for desired county
         cmd =  "WITH county AS (SELECT shape_id, name, shape FROM shapes \
                                 WHERE (shape && 'POINT(%s %s)' :: geometry)\
-                                AND ST_CONTAINS(shape, 'POINT(%s %s)' :: geometry)\
+                                AND ST_CONTAINS(shape, \
+                                    'POINT(%s %s)' :: geometry)\
                                 AND type = 'County'\
                 LIMIT 1)\
                 SELECT (SELECT name from county), \
@@ -354,43 +311,42 @@ def US_SIF_tab():
                 ORDER BY day;" \
                 % (coord_x, coord_y, coord_x, coord_y)
 
-        # Execute query and obtain results
-        cursor.execute(cmd)
+        # Obtain results
+        result = query_db(cmd)
 
-        # Array to store the series of obtained results
-        dates = []
-        sifs = []
+        # If empty, clear and return
+        if len(result) == 0:
+            sif_series.title.text = "SIF Time-Series (Select a county...)"
+            time_srs_src.data = dict(date=[], sif=[])
+            return
 
-        # Get the county's name
-        county_name = ''
-
-        # Loop through each record and get the info you need
-        for name, date, avg in cursor:
-            county_name = name
-            dates.append(date)
-            sifs.append(float(avg))
+        # Map the rows to columns and take series
+        mapped_result = [list(i) for i in zip(*result)]
+        county_name, dates, sifs = (result[0][0], mapped_result[1], 
+                                    mapped_result[2])
 
         # Set the title of the Time Series plot to reflect the selected county
         sif_series.title.text = "SIF Time-Series in County: %s" % (county_name)
 
         # Set the appropriate data source
-        time_srs_src1.data = dict(date=dates, sif=sifs)
+        time_srs_src.data = dict(date=dates, sif=sifs)
 
     # Source of the time-series data should be empty for now
-    time_srs_src1 = ColumnDataSource(data=dict(date=[], sif=[]))
+    time_srs_src = ColumnDataSource(data=dict(date=[], sif=[]))
 
     # Which tools should be available to the user for the timer series
     TOOLS = "pan,wheel_zoom,reset,hover,save,tap"
 
     # Figure that holds the time-series
     sif_series = figure(plot_width=800, plot_height=400, x_axis_type='datetime',
-                        tools=TOOLS, title= "SIF Time-Series (Select county..)",
+                        tools=TOOLS, 
+                        title= "SIF Time-Series (Select a county...)",
                         active_scroll = "wheel_zoom",
                         x_axis_label = 'Date',
                         y_axis_label = 'SIF Average')
 
     sif_series.scatter('date', 'sif', 
-                        source=time_srs_src1, color = 'green')
+                        source=time_srs_src, color = 'green')
 
     # Some font choices
     sif_series.title.text_font_size = '16pt'
