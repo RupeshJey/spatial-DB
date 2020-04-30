f742a0ac8fa6c2d2bedc28c36a98394d0b689d0b
RupeshJey <rupeshjey@gmail.com>
2020-04-30 10:35:13 -0700

    Enable server respond to query params

--- Spatial_Server/spatial_server.jl
+++ Spatial_Server/spatial_server.jl
@@ -1,11 +1,17 @@
-using Genie
+using Genie, Genie.Router, DataFrames, LibPQ
 import Genie.Router: route
 import Genie.Renderer.Json: json
 
+conn = LibPQ.Connection("dbname=spatial_db user=explorer password=outsider port=65432")
+
+
 Genie.config.run_as_server = true
 
-route("/") do
-  (:message => "Hi there!") |> json
+route("/shapes") do
+  level = haskey(@params, :level) ? @params(:level) : 0
+  println(level)
+  df = DataFrame(execute(conn, "SELECT name_0 AS name FROM level$level"))
+  (:message => "Hi there $df.name") |> json
 end
 
-Genie.startup()
+Genie.startup(5055, "0.0.0.0", ws_port=5055)
